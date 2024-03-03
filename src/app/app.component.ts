e5cff8f7808a780dbf9fcf368ac0f3807b128ea5
Anushka Verma <anushkaverma779@gmail.com>
2024-03-03 19:10:55 +0530

    Done

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { CommonService } from './common.service';
+declare var $:any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'Weather';
+  IsChangeLocation=false;
+  TodayDate: Date;
+  weatherData: any;
+  cityName: any;
+
+  constructor(private service:CommonService){
+    this.TodayDate = new Date();
+  }
+
+  changeLocation(){
+    this.IsChangeLocation = this.IsChangeLocation==true?false:true;
+  }
+
+  getWeatherDataByCity(){
+    var city = $("#CityName").val();
+    this.service.getWeatherData(city).subscribe(data=>{
+      this.cityName = city;
+      this.weatherData = data;
+    })
+  }
+  transform(value: number): number {
+    return Math.round(value);
+  }
+}
